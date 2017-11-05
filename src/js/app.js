db04bba85450d2256e51b0a34e14d2f68a50c921
Vladimir Simonovski <vlsim@outlook.com>
2017-11-05 01:16:45 +0100

    FE

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,13 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
 import './css_modules';
+
+import Landing from './components/Landing';
+
+class Home extends Component {
+    render() {
+        return <Landing />;
+    }
+}
+
+render(<Home />, document.getElementById('app'));
---
+++ src/js/components/Plant.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export class Plant extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    render() {
+        return (
+            <div className="landing__plants__single landing__card">
+                aaaaaaaaaaaaaaa{' '}
+            </div>
+        );
+    }
+}
+
+export default Plant;
